0dbcb93549a1dd122eadc961d25729f5ae7bbffa
VectoDE <system.bot.discord.utility@gmail.com>
2025-10-22 17:01:06 +0200

    Fix Windows process constants

--- internal/storage/process_windows.go
+++ internal/storage/process_windows.go
@@ -7,14 +7,21 @@ import (
 	"syscall"
 )
 
+const (
+	processQueryLimitedInformation = 0x1000
+	stillActive                    = 259
+)
+
+var errInvalidParameter = syscall.Errno(87)
+
 func isProcessRunning(pid int) bool {
 	if pid <= 0 {
 		return false
 	}
 
-	handle, err := syscall.OpenProcess(syscall.PROCESS_QUERY_LIMITED_INFORMATION, false, uint32(pid))
+	handle, err := syscall.OpenProcess(processQueryLimitedInformation, false, uint32(pid))
 	if err != nil {
-		if errors.Is(err, syscall.ERROR_INVALID_PARAMETER) {
+		if errors.Is(err, errInvalidParameter) {
 			return false
 		}
 		// If access is denied or another error occurs, assume the process may still exist.
@@ -27,5 +34,5 @@ func isProcessRunning(pid int) bool {
 		return true
 	}
 
-	return exitCode == syscall.STILL_ACTIVE
+	return exitCode == stillActive
 }
